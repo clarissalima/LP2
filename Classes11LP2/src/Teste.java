9d520fdc7030c8600aa5695984bb36588446b9e7
Clarissa Lima <73674908+clarissalima@users.noreply.github.com>
2023-06-01 21:04:28 -0300

    Add files via upload

---
+++ Classes11LP2/src/Teste.java
@@ -0,0 +1,22 @@
+
+public class Teste {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		TiqueTaque tt = new TiqueTaque();
+		ThreadTiqueTaque tique = new ThreadTiqueTaque("Tique", tt);
+		ThreadTiqueTaque taque = new ThreadTiqueTaque("Taque", tt);
+		
+		try {
+			tique.t.join();
+			taque.t.join();
+		} catch (InterruptedException e) {
+			
+			e.printStackTrace();
+		}
+
+	}
+
+}
